0e30edf98f1b7573562cab431a23a79f9bc23981
Yamiluster <cariagra.15@gmail.com>
2021-09-25 17:08:21 -0500

    Actualizado para subir a Heroku

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 app.use(express.static('public'));
 
 
-app.listen(3000, ()=>{
+app.listen(process.env.PORT || 3000, ()=>{
     console.log('Servidor todo chido en el puerto 3000');
 });
 
